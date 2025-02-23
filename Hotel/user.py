eaaccb7e129118b7ae03f996b210e110d55a2d97
zUlisesz <zperplexz@gmail.com>
2025-02-23 16:34:50 -0600

    Update user.py

--- Hotel/user.py
+++ Hotel/user.py
@@ -4,41 +4,57 @@ class Usuario:
     usuarios = []
 
     def __init__(self, nombre, rol, password):
+
         self.nombre = nombre
         self.rol = rol
         self.password = password
         Usuario.usuarios.append(self)
-        
-    def getInfo(self):
-        return "Nombre :{}, Rol: {}\n".format(self.nombre, self.rol)
+
+    def get_info(self):
+        return f"Nombre: {self.nombre}, Rol: {self.rol}\n"
 
     @staticmethod
-    def cargarUsuarios(file):
+    def cargar_usuarios(filename):
         try:
-            with open(file, 'r') as file:
+            with open(filename, 'r', encoding='utf-8') as file:
                 data = json.load(file)
                 for user_data in data:
-                    Usuario(user_data['nombre'], user_data['rol'], user_data['password'])
+                    required_fields = ['nombre', 'rol', 'password']
+                    if all(field in user_data for field in required_fields):
+                        Usuario(
+                            nombre=user_data['nombre'],
+                            rol=user_data['rol'],
+                            password=user_data['password']
+                        )
+                    else:
+                        print(f"Skipping invalid user data: {user_data}")
         except FileNotFoundError:
-            pass
+            print("Archivo {} no encontrado".format(filename))
+            
 
     @staticmethod
-    def guardarUsuarios(file):
+    def guardar_usuarios(filename):
         try:
-            with open(file, 'r') as file:
+            with open(filename, 'r', encoding='utf-8') as file:
                 existing_data = json.load(file)
         except FileNotFoundError:
             existing_data = []
 
+        new_users = []
         for user in Usuario.usuarios:
             user_data = {
                 'nombre': user.nombre,
                 'rol': user.rol,
                 'password': user.password
             }
-            existing_data.append(user_data)
+            new_users.append(user_data)
 
-        with open(file, 'w') as file:
-            json.dump(existing_data, file, indent=4)
+        existing_data.extend(new_users)
 
-        Usuario.usuarios.clear()
+        try:
+            with open(filename, 'w', encoding='utf-8') as file:
+                json.dump(existing_data, file, indent=4, ensure_ascii=False)
+            Usuario.usuarios.clear()
+            print("Datso enviados correctamente a: {}".format(filename))
+        except Exception as e:
+            print("{}".format(e))
