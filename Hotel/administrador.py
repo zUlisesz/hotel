5669297982be2511f173b680bf9891a423bb6357
zUlisesz <zperplexz@gmail.com>
2025-02-24 08:35:55 -0600

    Update administrador.py

---
+++ Hotel/administrador.py
@@ -0,0 +1,13 @@
+class Administrador:
+
+    def registrar(self):
+        pass
+
+    def modificarCliente(sefl , cliente):
+        pass
+
+    def eliminarUsuario(sefl , cliente):
+        pass
+
+    def crearRecamara(sefl, huesped):
+        pass
