5ac0e59493e8b2fe06467ea208f73f4364a6576e
zUlisesz <zperplexz@gmail.com>
2025-02-26 08:36:02 -0600

    Update reserva.py

--- Hotel/reserva.py
+++ Hotel/reserva.py
@@ -1,12 +1,17 @@
-class Rerserva:
-    reservas = []
-    def __init__(self,id , cuarto, fechaInicio, fechaFin):
-        self.id = id
-        self.ciuarto = cuarto
-        self.fechaInicio = fechaInicio
-        self.fechaFin = fechaFin
-        Rerserva.reservas.append(self)
+from datetime import datetime  
 
-    def costoTotal(self , id):
-        total = id.costo * (self.fechaFin - self.fechaInicio)
-        return "Costo total de la habitacion {}".format(total)
+class Reserva:
+    lista_reservas = []
+
+    def __init__(self, id, habitacion, fecha_entrada, fecha_salida, huesped):
+        self.id = id
+        self.habitacion = habitacion
+        self.fecha_entrada = datetime.strptime(fecha_entrada, "%Y-%m-%d").date()  
+        self.fecha_salida = datetime.strptime(fecha_salida, "%Y-%m-%d").date()    
+        self.huesped = huesped
+        Reserva.lista_reservas.append(self)
+
+    def costo_total(self):
+        dias = (self.fecha_salida - self.fecha_entrada).days  
+        costototal = self.habitacion.costo * dias
+        return f"El costo total de la habitación {self.habitacion.numero} será {costototal}."
