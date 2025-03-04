4786f31a00a0c7f13dd679a38eb00f9b1e91eefb
zUlisesz <zperplexz@gmail.com>
2025-03-04 11:31:43 -0600

    Update interfaz.py
    - Ventana principal terminada a nivel gráfico y funcional.
    - Ventana de administrador terminada a nivel gráfico, a nivel funcional no está terminado( el botón 'OLVER AL INICIO' ya tiene funcionalidad.

--- Hotel/interfaz.py
+++ Hotel/interfaz.py
@@ -4,34 +4,43 @@ from PIL import Image , ImageTk
 from user import Usuario
 from huesped import Huesped
 from administrador import Administrador
+from validacion import Validation
 
 def loadfiles():
-    
-    if( Huesped.cargarHuepedes('huespedes.json') == True):
-        print('data loaded successfuly')
-    else:
-        print('not having done that correctly, take this\n')
+    Huesped.cargarHuepedes('huespedes.json')        
+    Usuario.cargar_usuarios('users.json')
     
 
 def ventanaAdmin():
     global windowAdmin
     window.withdraw()
-    windowAdmin = Tk()
-    windowAdmin.title('Ventana de administrador ')
+    windowAdmin = Toplevel()
+    windowAdmin.title('Ventana de administrador')
+    windowAdmin.config(bg =  'AliceBlue')
+    windowAdmin.resizable(width=False , height= False)
+    
+    admin = Image.open("C:/Users/PC/Desktop/hotel-main/hotel-main/Hotel/imgAdministrador-1.jpg").resize((560,424))
+    adminTk = ImageTk.PhotoImage(admin)
+    labelAdmin = Label(windowAdmin, image= adminTk)
+    
 
-    adminLabel = Label(windowAdmin, text = 'Bienvenido {}'.format(ulises.nombre), font = ('Agency Fb',24, 'bold'), padx = 40, pady = 6, bg= '#ADD8E6' )
+    adminLabel = Label(windowAdmin, text = f'BIENVENIDO {(ulises.nombre).upper()}, HAS ACCEDIDO COMO ADMINISTRADOR', font = ('Agency Fb',30, 'bold'), padx = 30, pady = 6)
 
-    botonCrear  = Button(windowAdmin , text = 'Crear Usuario', font = ('Agency Fb',20, 'bold'), border = 4, pady = 3, command=ventanaAdminAggHuesped)
-    botonEliminar  = Button(windowAdmin, text = 'Eliminar Usuario', font = ('Agency Fb',20, 'bold'), border = 4, pady = 3 )
-    botonModificar  = Button(windowAdmin , text = 'Modificar usuario', font = ('Agency Fb',20, 'bold'), border = 4, pady = 3)
-    botonCrearRecamara  = Button(windowAdmin , text = 'Crear recamara', font = ('Agency Fb',20, 'bold'), border = 4, pady = 3)
+    botonCrear  = Button(windowAdmin , text = 'Crear Usuario'.upper() ,bg = 'CadetBlue3' ,font = ('Agency Fb',20, 'bold'), border = 4,padx = 26,  pady = 3, command = ventanaAdminAggHuesped)
+    botonEliminar  = Button(windowAdmin, text = 'Eliminar Usuario'.upper(), bg = 'CadetBlue3',font = ('Agency Fb',20, 'bold'), border = 4, padx = 16 , pady = 3 )
+    botonModificar  = Button(windowAdmin , text = 'Modificar usuario'.upper() ,bg = 'CadetBlue3', font = ('Agency Fb',20, 'bold'), border = 4,padx = 10 ,  pady = 3)
+    botonCrearRecamara  = Button(windowAdmin , text = 'Crear recamara'.upper() ,bg = 'CadetBlue3', font = ('Agency Fb',20, 'bold'), border = 4,padx = 16 ,  pady = 3)
+    botonVolver = Button(windowAdmin , text = 'Volver al inicio'.upper() ,bg = 'CadetBlue3', font = ('Agency Fb',20, 'bold'), border = 4,padx = 22,  pady = 3, command = lambda: volverInicio(windowAdmin))
 
-    adminLabel.grid(row = 0 , column= 0 ,columnspan= 2, pady = 18 )
+    adminLabel.pack(pady = 18 )
 
-    botonCrear.grid( row = 1, column= 0 , padx = 24, pady = 30 )
-    botonEliminar.grid(row = 2, column= 0 , padx = 24 , pady = 30)
-    botonModificar.grid(row  = 1, column= 1, padx = 24 , pady = 30)
-    botonCrearRecamara.grid(row = 2 , column= 1, padx = 24 , columnspan= 30)
+    labelAdmin.pack(side= 'left', padx = 20)
+    
+    botonCrear.pack( padx = 20, pady = 11  )
+    botonEliminar.pack(padx = 20 , pady = 11 )
+    botonModificar.pack(padx = 20 , pady = 11)
+    botonCrearRecamara.pack(padx = 20 , pady = 11 )
+    botonVolver.pack(padx = 20 , pady = 10 )
 
 
     windowAdmin.mainloop()
@@ -39,36 +48,38 @@ def ventanaAdmin():
 def ventanaInicio():
     global window
     window = Tk()
-    window.config(bg = "white")
+    window.config(bg = 'AliceBlue')
     window.title('Log in')
+    window.resizable(width= False, height= False)
 
-    login = Image.open("C:/Users/PC/Desktop/hotel-main/hotel-main/Hotel/icon.jpg")
-    login = login.resize((200,200))
+    login = Image.open("C:/Users/PC/Desktop/hotel-main/hotel-main/Hotel/imagenHotel.jpg").resize((440,260))
     loginTk = ImageTk.PhotoImage(login)
-    loginLabel  = Label(window, image= loginTk)
+    loginLabel  = Label(window , image= loginTk)
 
     global nombreEntry
     global passEntry
     
-    nombreLabel = Label(window, text = 'Usuario', font = ('Agency Fb',18, 'bold'), border= 2)
-    passLabel = Label(window, text = 'Contraseña', font = ('Agency Fb',18, 'bold'),border = 2)
+    nombreLabel = Label(window, text = 'USUARIO', font = ('Agency Fb',18, 'bold'))
+    passLabel = Label(window, text = 'CONTRASEÑA', font = ('Agency Fb',18, 'bold'))
 
-    nombreEntry = Entry(window, width= 24, font= ("Agency Fb",24))
-    passEntry = Entry(window, width  = 24 , show= '*', font = ("Agency Fb", 24))
+    nombreEntry = Entry(window, width= 24, font= ("Agency Fb",20), border = 4)
+    passEntry = Entry(window, width  = 24 , show= '*', font = ("Agency Fb", 20), border = 4)
 
-    boton = Button(window , text = 'Inicio', font = ('Agency Fb',20, 'bold'),border  = 4,  padx = 46, pady = 2, command= fboton)
+    botonEntrar = Button(window , text = 'ENTRAR', font = ('Agency Fb',20, 'bold'),bg = 'PaleGreen',border  = 4 , padx = 94, pady = 2, command= fboton )
+    botonSalir  = Button(window , text = 'SALIR', font = ('Agency Fb',20, 'bold'),bg = 'PaleVioletRed',border  = 4,  padx = 104, pady = 2, command= window.quit)
 
+    loginLabel.grid(row= 0 , column= 0 , columnspan= 2, padx = 10, pady =14 )
 
-    loginLabel.grid(row= 0 , column= 0 , columnspan= 2, padx = 60, pady =10 )
+    nombreLabel.grid( row = 1 , column = 0  , pady = 18)
+    passLabel.grid(row = 2 , column = 0 , pady = 18 )
 
-    nombreLabel.grid( row = 1 , column = 0 , padx = 20 , pady = 30)
-    passLabel.grid(row = 2 , column = 0, padx = 20 , pady = 30 )
-
-    nombreEntry.grid( row = 1 , column= 1 , padx = 20 ,pady  = 30 )
-    passEntry.grid(row  = 2, column= 1, padx = 20 , pady = 30)
-
-    boton.grid(row = 3, column= 0, columnspan= 2 , padx = 20 , pady = 14)
-
+    nombreEntry.grid( row = 1 , column= 1,pady  = 18 )
+    passEntry.grid(row  = 2, column= 1, pady = 18)
+    
+    
+    botonEntrar.grid(row = 3, column= 1, columnspan= 2 , pady = 14)
+    botonSalir.grid(row = 4, column= 1, columnspan= 2, pady = 20)
+    
     window.mainloop()
 
 
@@ -119,22 +130,39 @@ def ventanaRecepcionista():
     windowRecepcionista = Tk()
     window .title('Ventana e recepción')
     
-    windowRecepcionista.mainloop()    
+    #windowRecepcionista.mainloop()    
     
+def volverInicio(currentScreen):
+    currentScreen.withdraw()
+    window.deiconify()
+    nombreEntry.delete( 0 , END)
+    passEntry.delete(0 , END)
 
 def fboton():
     nombre = nombreEntry.get()
     password = passEntry.get()
-    user = Usuario.iniciar_sesion(nombre , password)
-    if user.rol == 'administrador':
-        ventanaAdmin()
-    elif user.rol  == "recepcionista":
-        ventanaRecepcionista()
+    
+    if (Validation.usuarioExistente(Usuario.usuarios, nombre)):
+        
+        if (Validation.camposCompletos(nombre , password) ):   
+                    
+            user = Usuario.iniciar_sesion(nombre , password)
+            if user.rol == 'administrador':
+                ventanaAdmin()
+            elif user.rol  == "recepcionista":
+                ventanaRecepcionista()
+            else:
+                messagebox.showerror("Ups!", "Parece que no farmas parte del equipo de administradores\no no formas parte del equipo de recepcionistas")
+        else:
+            messagebox.showerror('Empty Fields','Completar todos los campos')
     else:
-        messagebox.showerror("Ups!", "Acceso denegado")
-
+        nombreEntry.delete(0,END)
+        messagebox.showerror('Not found','Usuario no existente')
 
 ulises = Administrador('Ulises','administrador', 'marshal')
+carlos = Usuario('Carlos','recepcionista','utf')
+
+ventanaInicio()
 carlos = Usuario('Carlos','recepcionista','utf')
 
 loadfiles()
